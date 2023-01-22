e54d98bbcc3f92f63079503840123b1b677cf562
Naman Sinha <65483393+Naman794@users.noreply.github.com>
2023-01-22 23:44:09 +0530

    Main source code

---
+++ src/chat.py
@@ -0,0 +1,48 @@
+import discord 
+from discord.ext import commands
+
+class GPT3Cog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.conn = sqlite3.connect('chatbot.db')
+        self.cursor = self.conn.cursor()
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS chat_log (
+            id INTEGER PRIMARY KEY,
+            user_input TEXT,
+            api_response TEXT,
+            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+        )''')
+        self.conn.commit()
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.channel.id == 'YOUR_CHANNEL_ID' and message.author != self.bot.user:
+            match = re.search(r"^\/chat (.*)", message.content)
+            if match:
+                query = match.group(1)
+                try:
+                    response = generate_text(query)
+                    await message.channel.send(response)
+                    self.insert_data(query, response)
+                except:
+                    self.study_command(query)
+                    await message.channel.send("Sorry, I didn't understand your command. Let me look into it.")
+
+    def insert_data(self, user_input, api_response):
+        self.cursor.execute("INSERT INTO chat_log (user_input, api_response) VALUES (?,?)", (user_input, api_response))
+        self.conn.commit()
+
+    def study_command(self, command):
+        self.cursor.execute("SELECT user_input, api_response FROM chat_log WHERE user_input LIKE ?", ('%'+command+'%',))
+        rows = self.cursor.fetchall()
+        if rows:
+            for row in rows:
+                user_input, api_response = row
+                # Study the command and improve it
+                print(f"user_input: {user_input}, api_response: {api_response}")
+                # code to study and improve command
+        else:
+            print(f"No previous data found for command: {command}")
+
+async def setup(bot):
+	await bot.add_cog(GPT3Cog(bot))
